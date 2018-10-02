93163e974a8b320aca9817c5bd2da698efaa00b8
Jules Kuehn <jk@jules.lol>
2018-10-01 21:14:23 -0400

    Added experimental rank-finding code to Question 3

--- A1/q3-jules.py
+++ A1/q3-jules.py
@@ -18,10 +18,8 @@ A = np.fromfunction(g, (1401, 1401))
 
 U, s, V = sp.svd(A, full_matrices=True)
 
-print(s)
-
 # Diagonalize s vector to matrix
-s = np.diag(s)
+sDiag = np.diag(s)
 
 print("\nA =\n", A)
 
@@ -29,12 +27,34 @@ print("\nA =\n", A)
 # since s is sorted descending
 U2 = U[:, :2]
 V2 = V[:2, :]
-s2 = s[:2, :2]
+s2 = sDiag[:2, :2]
 
 A2 = np.dot(U2, s2).dot(V2)
 
 print("\nA2 =\n", A2)
 
 diff = np.linalg.norm(A-A2)
-print("\n||A - A2|| =\n", diff)
+print("\n||A - A2|| =", diff)
 
+# Accuracy of this approximation is very good
+print(f"\n||A - A2|| / ||A|| = {diff*100 / np.linalg.norm(A):5.3f}%")
+
+""" # What rank of matrix contains >= 99.9% energy?
+sumS = np.sum(s)
+
+print(sumS)
+k = 0
+e = 0
+while e < sumS * 0.999:
+    e += s[k]
+    k += 1
+
+print("\nbest k =", k)
+
+Ak = np.dot(U[:, :k], sDiag[:k, :k]).dot(V[:k, :])
+
+print(f"\nA{k} =\n", Ak)
+
+diff = np.linalg.norm(A-Ak)
+print(f"\n||A - A{k}|| =\n", diff)
+print(f"\n||A - A{k}|| / ||A|| = {diff / np.linalg.norm(A):10.10}") """
