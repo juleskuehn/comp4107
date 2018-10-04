cd0ac8d5e415f20be0778a51df2d677a713bec92
Jules Kuehn <jk@jules.lol>
2018-10-03 20:21:20 -0400

    Question 4 lots of printout (show movement)

--- A1/q4-jules.py
+++ A1/q4-jules.py
@@ -13,46 +13,45 @@ A = np.array([
     [4, 5, 6],
     [1, 1, 1]
 ])
+b = np.array([1,1,1,1]).transpose()
+X = np.array([1,1,1]).transpose()
 
-# x is a Vector
-# http://www.seas.ucla.edu/~vandenbe/133A/lectures/ls.pdf
-# and last slides on
-# https://sikaman.dyndns.org:8443/WebSite/rest/site/courses/4107/handouts/04-BasicAlgorithms.pdf
-#
-# If xHat == 0 , xHat solves Ax-b (rarely happens)
-# Least squares solution minimizes ||Ax-b||^2
-# Some values of step size will not have solutions
-# because it will overshoot solutions
 tolerance = 0.01
 steps = [0.001, 0.005, 0.01, 0.015, 0.02, 0.025, 0.05]
-b = np.array([1,1,1,1]).transpose()
+
 At = A.transpose()
 AtA = np.dot(At, A)
-
-# print(b)
-# print(At)
-# print(AtA)
-
 Apinv = np.linalg.pinv(A)
-X = np.array([0,0,0]).transpose()
+
 def computeLoss():
     return np.dot(AtA, x) - np.dot(At, b)
+
+def arrToString(arr):
+    s = ""
+    for el in arr:
+        s += f"{el:>12.6f}"
+    return s
 
 x = X
 L = computeLoss()
 i = 0
+
+print("|    Step     |                  x                 |  Iter  |")
+print("=============================================================")
+print(" P-inv sol'n:", arrToString(np.dot(Apinv, b)))
+
 for step in steps:
     x = X
     L = computeLoss()
     i = 0
-    print(f"step = {step}:")
-    while np.linalg.norm(L) > tolerance:
+    while np.linalg.norm(np.dot(AtA, x)**2 - np.dot(At, b)**2) > tolerance:
         i += 1
         L = computeLoss()
         x = x - step*L
-        # if i%100 == 0:
-        #     print(i, x)
-    print(i, x)
+        if (np.linalg.norm(L) > 100000):
+            break
+        if True:
+            print(f" step: {step:.3f}: {arrToString(x)}   {i}")
+    print(f" step: {step:.3f}: {arrToString(x)}   {i}")
 
-print("Pseudo-inverse solution = ", np.dot(Apinv, b))
 # print(L)
