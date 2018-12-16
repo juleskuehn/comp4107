9bd7fbd1923a5106d6af9cb159342c9e718c9cf2
Jules Kuehn <jk@jules.lol>
2018-12-16 15:28:59 -0500

    Finished writing, results, need to do appendices and editing

--- project/document/code/kerasModel.py
+++ project/document/code/kerasModel.py
@@ -1,5 +1,5 @@
 model = Sequential()
-model.add(Embedding(vocab_size, 32, input_length=max_title_length))
+model.add(Embedding(vocab_size, embed_size, input_length=max_title_length))
 model.add(Flatten())
 model.add(Dropout(0.5))
 model.add(Dense(32, activation='softmax'))
@@ -7,4 +7,4 @@ model.compile(
     loss='categorical_crossentropy',
     optimizer=Adam())
 model.fit(
-    trX, to_categorical(trY), epochs=5)
+    trX, to_categorical(trY), epochs=num_epochs)
